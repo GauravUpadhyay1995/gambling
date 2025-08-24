3c654ca0e405b56f2013d36b62fcbe1537712b69
GAURAV UPADHYAY <142603807+GauravUpadhyay1995@users.noreply.github.com>
2025-08-24 17:05:42 +0530

    add cookie

--- src/app/admin/customers/page.tsx
+++ src/app/admin/customers/page.tsx
@@ -1,6 +1,7 @@
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import CustomersListTable from "@/components/tables/CustomersListTable";
 import { Metadata } from "next";
+import { cookies } from "next/headers";
 
 
 export const metadata: Metadata = {
@@ -9,9 +10,20 @@ export const metadata: Metadata = {
 };
 
 export default async function CustomersTables() {
+
+      const cookieStore = await cookies();
+      const cookieHeader = cookieStore
+        .getAll()
+        .map((cookie) => `${cookie.name}=${cookie.value}`)
+        .join("; ");
 const url=`${process.env.NEXT_PUBLIC_CUSTOMER_API_URL}/list?perPage=25`
 
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    headers: {
+      Cookie: cookieHeader,
+    },
+    cache: 'no-store', // ensures fresh server data
+  });
   console.log("url>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", url);
 
   const result = await res.json();
